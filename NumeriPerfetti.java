39d74fa35fb5f7978d34ffb78bdc0608a4f335db
BotSupremo <tonsuso.michael@itarchimede.it>
2025-04-05 08:56:34 +0200

    modifica del file numeriPerfetti

--- NumeriPerfetti.java
+++ NumeriPerfetti.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
+
+// commenti esercitazione 05/04/2025
+// classe Main
 public class Main
 {
+    // metodo main
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
+        // variabili
 		int somma = 0;
+       
         
         System.out.print("Inserisci un numero: ");
         int N = scanner.nextInt();
         
         
         
-        
+        // ciclo for
         for (int i = 1; i < N; i++){
             
             if (N % i == 0 && i != N){
@@ -20,7 +26,7 @@ public class Main
 
         }
         
-        
+        // condizioni
         if (somma == N){
             System.out.println("Il numero e' perfetto");
         }
